c81528eb31332eabe52d98aef00cf39cfcdbd121
ghinwaA22 <110889396+ghinwaA22@users.noreply.github.com>
2022-09-01 14:16:54 -0700

    New one

--- tar/urls.py
+++ tar/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SectionList, SectionView, ChapterList
+from .views import SectionList, SectionView, ChapterList, ItemList
 
 urlpatterns = [
     path('', SectionList.as_view(), name='section_list'),
     path('add/', SectionView.as_view(), name='add-section'),
     path('<int:section_id>/', ChapterList.as_view(), name='chapter_list'),
-    path('<int:section_id>/', ChapterList.as_view(), name='chapter_list'),
+    path('section/<int:chapter_id>/', ItemList.as_view(), name='item_list'),
 
 ]
--- tar/views.py
+++ tar/views.py
@@ -42,4 +42,15 @@ class ChapterList(ListView):
 
 class ItemList(ListView):
     model = Item
+    template_name = 'item_list.html'
+    context_object_name = 'items'
+
+    def get_queryset(self, *args, **kwargs):
+        query = Item.objects.filter(chapter_id=self.kwargs.get('chapter_id'))
+        return query
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['page_title'] = 'items'
+        return context
 
